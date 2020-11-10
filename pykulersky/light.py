e63c6b7583bca055ff3d47c8c0f674f1bc6e113e
Emily Mills <emily@emlove.me>
2020-11-10 11:27:45 -0600

    Workaround for firmware bug

--- pykulersky/light.py
+++ pykulersky/light.py
@@ -76,12 +76,22 @@ class Light():
                 raise ValueError(
                     "Value {} is outside the valid range of 0-255")
 
+        old_color = self.get_color()
+        was_on = max(old_color) > 0
+
         _LOGGER.info("Changing color of %s to #%02x%02x%02x%02x",
                      self.address, r, g, b, w)
 
         if r == 0 and g == 0 and b == 0 and w == 0:
             color_string = b'\x32\xFF\xFF\xFF\xFF'
         else:
+            if not was_on and w > 0:
+                # These lights have a firmware bug. When turning the light on
+                # from off, the white channel is broken until it is first set
+                # to zero. If the light was off, first apply the color with a
+                # zero white channel, then write the actual color we want.
+                color_string = b'\x02' + bytes((r, g, b, 0))
+                self._write(CHARACTERISTIC_COMMAND_COLOR, color_string)
             color_string = b'\x02' + bytes((r, g, b, w))
 
         self._write(CHARACTERISTIC_COMMAND_COLOR, color_string)
--- tests/test_light.py
+++ tests/test_light.py
@@ -55,7 +55,9 @@ def test_set_color_not_connected(device):
     light = Light("00:11:22")
 
     with pytest.raises(PykulerskyException):
-        light.set_color(255, 0, 0, 0)
+        light._write(
+            '8d96b002-0002-64c2-0001-9acc4838521c',
+            b'\x02\x00\x00\x00\xFF')
 
 
 def test_set_color(device):
@@ -63,25 +65,49 @@ def test_set_color(device):
     light = Light("00:11:22")
     light.connect()
 
+    device.char_read.return_value = b'\x02\xFF\xFF\xFF\x00'
     light.set_color(255, 255, 255, 0)
     device.char_write.assert_called_with(
         '8d96b002-0002-64c2-0001-9acc4838521c',
         b'\x02\xFF\xFF\xFF\x00')
+    device.reset_mock()
 
+    device.char_read.return_value = b'\x02\xFF\xFF\xFF\x00'
     light.set_color(64, 128, 192, 0)
     device.char_write.assert_called_with(
         '8d96b002-0002-64c2-0001-9acc4838521c',
         b'\x02\x40\x80\xC0\x00')
+    device.reset_mock()
 
+    device.char_read.return_value = b'\x02\xFF\xFF\xFF\x00'
     light.set_color(0, 0, 0, 255)
     device.char_write.assert_called_with(
         '8d96b002-0002-64c2-0001-9acc4838521c',
         b'\x02\x00\x00\x00\xFF')
+    device.reset_mock()
 
     # When called with all zeros, just turn off the light
+    device.char_read.return_value = b'\x02\xFF\xFF\xFF\x00'
     light.set_color(0, 0, 0, 0)
     device.char_write.assert_called_with(
         '8d96b002-0002-64c2-0001-9acc4838521c', b'\x32\xFF\xFF\xFF\xFF')
+    device.reset_mock()
+
+    # Turn on only the RGB channels
+    device.char_read.return_value = b'\x32\xFF\xFF\xFF\xFF'
+    light.set_color(255, 255, 255, 0)
+    device.char_write.assert_called_with(
+        '8d96b002-0002-64c2-0001-9acc4838521c', b'\x02\xFF\xFF\xFF\x00')
+    device.reset_mock()
+
+    # Turn on white channel when previously off (test firmware workaround)
+    device.char_read.return_value = b'\x32\xFF\xFF\xFF\xFF'
+    light.set_color(255, 255, 255, 255)
+    assert device.char_write.call_args_list[0][0] == (
+        '8d96b002-0002-64c2-0001-9acc4838521c', b'\x02\xFF\xFF\xFF\x00')
+    assert device.char_write.call_args_list[1][0] == (
+        '8d96b002-0002-64c2-0001-9acc4838521c', b'\x02\xFF\xFF\xFF\xFF')
+    device.reset_mock()
 
     with pytest.raises(ValueError):
         light.set_color(999, 999, 999, 999)
@@ -96,11 +122,13 @@ def test_get_color(device, mocker):
     color = light.get_color()
     device.char_read.assert_called_with('8d96b002-0002-64c2-0001-9acc4838521c')
     assert color == (0, 0, 0, 255)
+    device.reset_mock()
 
     device.char_read.return_value = b'\x02\xFF\xFF\x00\x00'
     color = light.get_color()
     device.char_read.assert_called_with('8d96b002-0002-64c2-0001-9acc4838521c')
     assert color == (255, 255, 0, 0)
+    device.reset_mock()
 
     device.char_read.return_value = b'\x32\xFF\xFF\xFF\x00'
     color = light.get_color()
@@ -127,6 +155,7 @@ def test_exception_wrapping(device, adapter):
         light.connect()
         light.disconnect()
 
+    device.char_read.return_value = b'\x02\xFF\xFF\xFF\x00'
     device.char_write.side_effect = raise_exception
 
     with pytest.raises(PykulerskyException):
